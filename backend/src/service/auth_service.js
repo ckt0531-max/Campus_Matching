adfa155d6a49faf61818f94b7c86505491faa201
seo_ee <leeseoyun04@gmail.com>
2026-05-29 15:37:15 +0900

    feat: 회원가입/로그인 구현

---
+++ backend/src/service/auth_service.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+
+export const serializeUser = (user) => ({
+  id: user.id,
+  nick: user.nick,
+  preferredRole: user.preferredRole,
+  provider: user.provider || "local",
+});
+
+export const signAccessToken = (user) => {
+  const secretKey = process.env.JWT_SECRET || process.env.COOKIE_SECRET;
+
+  return jwt.sign(
+    {
+      sub: user.id,
+      nick: user.nick,
+      provider: user.provider,
+    },
+    secretKey,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+    }
+  );
+};
+
+export const extractBearerToken = (authHeader = "") => {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) return null;
+  return authHeader.split(" ")[1];
+};
